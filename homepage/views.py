44675d039278749ee97517f3bf2cc9eb061fdcab
xplanet95 <vladis.box@yandex.ru>
2021-09-11 21:37:35 +0800

    Homepage (index)
    Стартовая страница

---
+++ homepage/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'homepage/index.html')#render(request, 'homepage/index.html', context)
+
