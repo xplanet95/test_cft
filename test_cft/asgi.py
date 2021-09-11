8872e563737821e3b66db9630e3b2d776968014b
xplanet95 <vladis.box@yandex.ru>
2021-09-11 15:56:07 +0800

    Start Django project

---
+++ test_cft/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for test_cft project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_cft.settings')
+
+application = get_asgi_application()
